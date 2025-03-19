264d4a9551c05c366b398f2e5998df228d653281
xcc <xiaocydx@163.com>
2025-03-19 11:39:55 +0800

    feat: 添加stub模块

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,6 +38,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation(project(":performance-stub"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
---
+++ performance-stub/src/main/java/com/xiaocydx/performance/stub/MyClass.java
@@ -0,0 +1,4 @@
+package com.xiaocydx.performance.stub;
+
+public class MyClass {
+}
